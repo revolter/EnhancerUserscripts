f2c57a76d009983c924b4779bdd539d2e3928270
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-02-01 11:37:23 +0200

    Bumped Bandcamp Enhancer script's version to 0.2

--- scripts/Bandcamp/Bandcamp_Enhancer.user.js
+++ scripts/Bandcamp/Bandcamp_Enhancer.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Bandcamp Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.1
+// @version      0.2
 // @author       Iulian Onofrei
+// @updateURL    https://gist.github.com/raw/e4f1b561646cfa0b85a795da24e52a42/Bandcamp_Enhancer.user.js
 // @match        https://nord.bandcamp.com/album/era-marf
 // @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
 // @grant        GM_addStyle
