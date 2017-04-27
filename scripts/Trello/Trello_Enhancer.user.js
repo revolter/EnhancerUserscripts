da44ec9a14a45b065ac4de324c8af1b87252652d
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-04-27 11:15:36 +0300

    Added initial version of Trello Enhancer

---
+++ scripts/Trello/Trello_Enhancer.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name        Trello Enhancer
+// @namespace   http://iulianonofrei.com
+// @version     0.1
+// @author      Iulian Onofrei
+// @include     https://trello.com/*
+// @grant       none
+// ==/UserScript==
+
+document.head.innerHTML +=
+	"<style>\
+		.window {\
+			width: 70% !important;\
+		}\
+		.window-main-col {\
+			width: 90% !important;\
+		}\
+		.checklist {\
+			margin-bottom: 4px !important;\
+		}\
+	</style>";
+
+$(window).on("keypress", function(e) {
+	if(e.charCode === 63) {
+		addToggle();
+	}
+});
+
+function addToggle() {
+	$(".checklist").each(function(index, element) {
+	 var
+		 $element = $(element),
+		 $icon = $element.find(".window-module-title-icon");
+
+		if ($icon.css("cursor") !== "pointer") {
+			$icon.css("cursor", "pointer");
+
+			$icon.click(deselectChecklist);
+		}
+	});
+}
+
+function deselectChecklist() {
+	var
+		$element = $(this),
+		$checkboxes = $element.parent().parent().find(".checklist-item-state-complete .checklist-item-checkbox");
+
+	$checkboxes.each(function(index, element) {
+		setTimeout(function() {
+			$(element).click();
+		}, index * 200);
+	});
+}
