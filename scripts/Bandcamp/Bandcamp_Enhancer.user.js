fcab751a09bc7ecbba62e649554076f5fc63c51e
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-05-15 15:42:31 +0300

    Bumped Bandcamp Enhancer script's version to 0.4

--- scripts/Bandcamp/Bandcamp_Enhancer.user.js
+++ scripts/Bandcamp/Bandcamp_Enhancer.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Bandcamp Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.3
+// @version      0.4
 // @author       Iulian Onofrei
 // @updateURL    https://gist.github.com/raw/e4f1b561646cfa0b85a795da24e52a42/Bandcamp_Enhancer.user.js
 // @match        https://*.bandcamp.com/album/*
+// @match        https://*.bandcamp.com/releases
 // @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
 // @grant        GM_addStyle
 // ==/UserScript==
