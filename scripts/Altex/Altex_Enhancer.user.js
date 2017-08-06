c7eb5d59460088ca674d098a23b26f8cbe19ff47
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-08-07 00:14:00 +0300

    Bumped Altex Enhancer script's version to 0.3

--- scripts/Altex/Altex_Enhancer.user.js
+++ scripts/Altex/Altex_Enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Altex Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.2
+// @version      0.3
 // @author       Iulian Onofrei
 // @updateURL    https://gist.github.com/raw/7ac990d744d61db126742beefa49870c/Altex_Enhancer.user.js
 // @match        https://altex.ro/sales/order/history/
@@ -13,11 +13,15 @@
     'use strict';
 
     min.forEach(min.dom.getByXPath("//div[@class = 'my-account']/div[@class = 'u-container-reset']/table/tbody/tr", min.dom.ALL), function(order) {
-        var link = min.dom.getByXPath("/td/div/a", 0, order);
+        var
+            link = min.dom.getByXPath("/td/div/a", 0, order),
+            linkWrapper = order.lastElementChild;
 
         if (!link) {
             return;
         }
+
+        linkWrapper.rowSpan = "2";
 
         min.gm.xhr(link.href, function(doc) {
             var products = Array.from(min.dom.getByXPath("//table[@id = 'my-orders-table']/tbody/tr", min.dom.ALL, doc));
