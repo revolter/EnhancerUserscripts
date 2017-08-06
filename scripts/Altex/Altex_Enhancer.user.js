30e48b314fc4608f24d41eae525149ad052ef5f3
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-08-07 00:20:37 +0300

    Bumped Altex Enhancer script's version to 0.5

--- scripts/Altex/Altex_Enhancer.user.js
+++ scripts/Altex/Altex_Enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Altex Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.4
+// @version      0.5
 // @author       Iulian Onofrei
 // @updateURL    https://gist.github.com/raw/7ac990d744d61db126742beefa49870c/Altex_Enhancer.user.js
 // @match        https://altex.ro/sales/order/history/
@@ -12,11 +12,14 @@
 (function() {
     'use strict';
 
+    var confirmedOrderBackgroundColor = "rgba(0, 128, 0, 0.2)";
+
     min.forEach(min.dom.getByXPath("//div[@class = 'my-account']/div[@class = 'u-container-reset']/table/tbody/tr", min.dom.ALL), function(order) {
         var
             link = min.dom.getByXPath("/td/div/a", 0, order),
             linkWrapper = order.lastElementChild,
-            orderStatus = linkWrapper.previousElementSibling.firstElementChild;
+            orderStatus = linkWrapper.previousElementSibling.firstElementChild,
+            isOrderConfirmed = orderStatus.textContent === "Confirmat integral";
 
         if (!link) {
             return;
@@ -24,15 +27,15 @@
 
         linkWrapper.rowSpan = "2";
 
-        if (orderStatus.textContent === "Confirmat integral") {
-            order.style.backgroundColor = "rgba(0, 128, 0, 0.2)";
+        if (isOrderConfirmed) {
+            order.style.backgroundColor = confirmedOrderBackgroundColor;
         }
 
         min.gm.xhr(link.href, function(doc) {
             var products = Array.from(min.dom.getByXPath("//table[@id = 'my-orders-table']/tbody/tr", min.dom.ALL, doc));
 
             min.forEach(products, function(product) {
-                product.style.backgroundColor = "#EEE";
+                product.style.backgroundColor = isOrderConfirmed ? confirmedOrderBackgroundColor : "#EEE";
 
                 min.dom.insertAfter(product, order);
             });
