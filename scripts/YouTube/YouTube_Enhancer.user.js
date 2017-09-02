da008f66e9b27da8f953ac074698c2f083fb937d
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-09-02 20:22:12 +0300

    Bumped YouTube Enhancer script's version to 0.6

--- scripts/YouTube/YouTube_Enhancer.user.js
+++ scripts/YouTube/YouTube_Enhancer.user.js
@@ -1,70 +1,76 @@
 // ==UserScript==
-// @name		YouTube Enhancer
-// @namespace	http://iulianonofrei.com
-// @version		0.5
-// @author		Iulian Onofrei
-// @updateURL	https://gist.github.com/raw/c6ca9ed14d388e6e7e8278cebc3dfb29/YouTube_Enhancer.user.js
-// @match		https://youtube.com/*
-// @match		https://www.youtube.com/*
-// @require		https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
-// @grant		GM_addStyle
+// @name         YouTube Enhancer
+// @namespace    http://iulianonofrei.com
+// @version      0.6
+// @author       Iulian Onofrei
+// @updateURL    https://gist.github.com/raw/c6ca9ed14d388e6e7e8278cebc3dfb29/YouTube_Enhancer.user.js
+// @match        https://youtube.com/*
+// @match        https://www.youtube.com/*
+// @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
-	'use strict';
+    'use strict';
 
-	function setWide(isWide) {
-		if (isWide) {
-			document.body.classList.add("io-wide");
-		} else {
-			document.body.classList.remove("io-wide");
-		}
-	}
+    function setWide(isWide) {
+        if (isWide) {
+            document.body.classList.add("io-wide");
+        } else {
+            document.body.classList.remove("io-wide");
+        }
+    }
 
-	var isWide = min.dom.getByClassName("watch-wide");
+    function checkWide(element) {
+        if (!element.title) {
+            return;
+        }
 
-	setWide(isWide);
+        var isWide = element.title == "Default view";
 
-	min.dom.addObserver(function (mutations) {
-		var
-			node = mutations[0].target,
-			isWide = node.className.indexOf("watch-wide") !== -1;
+        setWide(isWide);
+    }
 
-		setWide(isWide);
-	}, min.dom.getById("page"), {attributes: true});
+    var isWide = min.dom.getByClassName("watch-wide");
 
-	min.gm.style({
-		".io-wide #masthead-positioner": {
+    setWide(isWide);
+
+    min.dom.onNodeExists(min.dom.getByClassName, "ytp-size-button", function(target) {
+        checkWide(target);
+
+        min.dom.addObserver(function (mutations) {
+            var element = mutations[0].target;
+
+            checkWide(node);
+        }, target, {attributes: true, childNodes: true});
+    });
+
+    min.dom.onNodeExists(min.dom.getById, "masthead-container", function(target) {
+        min.gm.style({
+            ".io-wide #top > #container": {
+                "margin-top": min.dom.getById("masthead-container").offsetHeight + "px"
+            }
+        });
+    });
+
+    min.gm.style({
+        ".io-wide #masthead-container": {
             "position": "absolute",
             "top": window.innerHeight + "px"
         },
-        ".io-wide #masthead-positioner-height-offset, .io-wide .skip-nav": {
-			"display": "none"
-		},
-		".io-wide #player": {
-			"height": window.innerHeight + "px"
-		},
-		".io-wide .player-height": {
-			"width": "100%",
-			"height": "100%",
+        ".io-wide #page-manager": {
+            "margin-top": "0"
+        },
+        ".io-wide #player": {
+            "max-height": window.innerHeight + "px"
+        },
+        ".io-wide .html5-video-container, .io-wide video": {
+            "width": "100%",
+            "height": "100%"
+        },
+        ".io-wide video": {
             "top": "0",
-			"left": "-50%"
-		},
-		".io-wide .html5-video-container": {
-			"height": "100%"
-		},
-		".io-wide .html5-main-video": {
-			"width": "100%",
-			"height": "100%",
-			"top": "0",
-			"left": "0"
-		},
-		".io-wide .ytp-chrome-bottom": {
-			"width": "100%",
-			"left": "0"
-		},
-        ".io-wide #content": {
-            "margin-top": "60px"
+            "left": "0"
         }
-	});
+    });
 })();
