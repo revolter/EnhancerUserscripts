f9a5251eb91cb2acd2b60dc436b9e44f0e8481ee
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-09-06 22:32:14 +0300

    Bumped YouTube Enhancer script's version to 0.8

--- scripts/YouTube/YouTube_Enhancer.user.js
+++ scripts/YouTube/YouTube_Enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.7
+// @version      0.8
 // @author       Iulian Onofrei
 // @updateURL    https://gist.github.com/raw/c6ca9ed14d388e6e7e8278cebc3dfb29/YouTube_Enhancer.user.js
 // @match        https://youtube.com/*
@@ -54,7 +54,7 @@
     });
 
     min.gm.style({
-        ".io-wide #masthead-container": {
+        ".io-wide ytd-app:not([guide-persistent-and-visible]) #masthead-container": {
             "position": "absolute",
             "top": window.innerHeight + "px"
         },
