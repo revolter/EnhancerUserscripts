2db9dfde2247c4d82c8f173f90ef8c6605c074cd
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-10-16 10:39:10 +0300

    Bumped Slashdot Enhancer script's version to 0.2

--- scripts/Slashdot/Slashdot_Enhancer.user.js
+++ scripts/Slashdot/Slashdot_Enhancer.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Slashdot Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.1
+// @version      0.2
 // @author       Iulian Onofrei
+// @updateURL    https://gist.github.com/revolter/3d83613164d1005ad08157c1eb4f37e0
 // @match        https://*.slashdot.org/*
 // @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
 // @grant        GM_addStyle
