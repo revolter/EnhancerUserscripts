77d18608acb79852b1ebbc3b31ac84648c1e03be
Iulian Onofrei <iulian.onofrei@yahoo.com>
2018-03-01 22:27:12 +0200

    Bumped Media Redirect script's version to 0.4

--- scripts/Media_Redirect/Media_Redirect.user.js
+++ scripts/Media_Redirect/Media_Redirect.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Media Redirect
 // @namespace    http://iulianonofrei.com
-// @version      0.3
+// @version      0.4
 // @author       Iulian Onofrei
 // @updateURL    https://gist.github.com/raw/930fc945fe0e8dc220ca040c8d1bafb3/Media_Redirect.user.js
 // @match        https://www.facebook.com/*
+// @match        https://www.google.com/imgres*
 // @match        https://www.instagram.com/*
 // @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
 // ==/UserScript==
@@ -21,10 +22,10 @@
             }
 
             min.dom.onNodeExists(min.dom.getByXPath, "//img[@class = 'spotlight']/@src", function(element) {
-                if (!element) {
-                    completion(null);
-                }
-
+                completion(element.value);
+            });
+        } else if (min.isOnWebsite("google.")) {
+            min.dom.onNodeExists(min.dom.getByXPath, "//meta[@property = 'og:image']/@content", function(element) {
                 completion(element.value);
             });
         } else if (min.isOnWebsite("instagram.com")) {
@@ -43,8 +44,10 @@
     }
 
     getRedirectURL(function(redirectURL) {
-        if (redirectURL) {
-            window.location.href = redirectURL;
+        if (!redirectURL) {
+            return;
         }
+
+        window.location.href = redirectURL;
     });
 })();
