c24bc13d1fa1491d43c8227ffd3b58093b13b2a7
Iulian Onofrei <iulian.onofrei@yahoo.com>
2018-05-21 00:25:12 +0300

    Bumped YouTube Enhancer script's version to 1.11.1

--- scripts/YouTube/YouTube_Enhancer.user.js
+++ scripts/YouTube/YouTube_Enhancer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         YouTube Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      1.11
+// @version      1.11.1
 // @author       Iulian Onofrei
-// @updateURL    https://gist.github.com/raw/c6ca9ed14d388e6e7e8278cebc3dfb29/YouTube_Enhancer.user.js
+// @updateURL    https://github.com/revolter/EnhancerUserscripts/raw/master/scripts/YouTube/YouTube_Enhancer.user.js
 // @match        https://youtube.com/*
 // @match        https://www.youtube.com/*
 // @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
