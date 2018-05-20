2e2f2ec90111369c78a96aa65102c2fd11e500ea
Iulian Onofrei <iulian.onofrei@yahoo.com>
2018-05-21 00:47:02 +0300

    Bumped OneSignal Enhancer script's version to 0.2.1

--- scripts/OneSignal/OneSignal_Enhancer.user.js
+++ scripts/OneSignal/OneSignal_Enhancer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        OneSignal Enhancer
 // @namespace   http://iulianonofrei.com
-// @version     0.2
+// @version     0.2.1
 // @author      Iulian Onofrei
-// @updateURL	https://gist.github.com/raw/db5d9936d7e7aa8b99f0908898674930/OneSignal_Enhancer.user.js
+// @updateURL	https://github.com/revolter/EnhancerUserscripts/raw/master/scripts/OneSignal/REPLACE_Enhancer.user.js
 // @match       https://onesignal.com/*
 // @require     https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
 // @grant       GM_addStyle
