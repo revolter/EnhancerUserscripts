374612fdaf5f51c21ffd29912c5366c01ae9e78e
Iulian Onofrei <iulian.onofrei@yahoo.com>
2018-05-21 01:09:31 +0300

    Bumped V LIVE Enhancer script's version to 0.6.1

--- scripts/V_LIVE/V_LIVE_Enhancer.user.js
+++ scripts/V_LIVE/V_LIVE_Enhancer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         V LIVE Enhancer
 // @namespace    http://iulianonofrei.com
-// @version      0.6
+// @version      0.6.1
 // @author       Iulian Onofrei
-// @updateURL    https://gist.github.com/raw/e798d141e0b0367a8e68c7c68372aa89/V_LIVE_Enhancer.user.js
+// @updateURL    https://github.com/revolter/EnhancerUserscripts/raw/master/scripts/V_LIVE/V_LIVE_Enhancer.user.js
 // @match        http://www.vlive.tv/video/*
 // @require      https://gist.githubusercontent.com/raw/dab432d4b4bbb672896b/min.js
 // ==/UserScript==
